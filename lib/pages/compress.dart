56c19c6bad34e6a66617b0233e435b917ec5ba4a
ChakornK <chakornk2007@gmail.com>
2025-12-04 18:44:26 -0800

    feat: allow custom video dimensions

--- lib/pages/compress.dart
+++ lib/pages/compress.dart
@@ -13,6 +13,7 @@ class _CompressPageState extends State<CompressPage> {
   List<String> log = [];
 
   num targetSize = 8;
+  num targetDimension = 1280;
 
   final ScrollController _controller = ScrollController();
 
@@ -22,49 +23,84 @@ class _CompressPageState extends State<CompressPage> {
       appBar: AppBar(title: const Text('Compress'), elevation: 1),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 16,
-          children: [
-            Text("Compression settings", style: Theme.of(context).textTheme.titleLarge),
-            SizedBox(
-              width: double.maxFinite,
-              child: TextFormField(
+        child: SizedBox(
+          width: double.maxFinite,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 16,
+            children: [
+              Text("Compression settings", style: Theme.of(context).textTheme.titleLarge),
+              TextFormField(
                 onChanged: (value) => setState(() {
-                  targetSize = num.parse(value);
+                  if (value.isEmpty) return;
+                  try {
+                    targetSize = num.parse(value.replaceAll(RegExp(r'[^\d]'), ""));
+                  } catch (_) {}
                 }),
+                validator: (value) {
+                  try {
+                    num.parse(value!);
+                    return null;
+                  } catch (_) {
+                    return "Invalid value";
+                  }
+                },
+                autovalidateMode: AutovalidateMode.always,
                 keyboardType: TextInputType.number,
                 initialValue: targetSize.toString(),
-                decoration: const InputDecoration(border: OutlineInputBorder(), isDense: true, suffixText: "MB", labelText: "Target size"),
+                decoration: const InputDecoration(border: OutlineInputBorder(), isDense: true, suffixText: "MB", labelText: "Maximum file size"),
               ),
-            ),
-            Divider(),
-            Text("Log", style: Theme.of(context).textTheme.titleLarge),
-            Expanded(
-              child: SizedBox(
-                width: double.maxFinite,
-                child: SingleChildScrollView(
-                  controller: _controller,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 80.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: log.map((ln) => ln.trim().isEmpty ? const Divider() : Text(ln, style: TextStyle(fontFamily: "monospace"))).toList(),
+              TextFormField(
+                onChanged: (value) => setState(() {
+                  if (value.isEmpty) return;
+                  try {
+                    targetDimension = num.parse(value.replaceAll(RegExp(r'[^\d]'), ""));
+                  } catch (_) {}
+                }),
+                validator: (value) {
+                  try {
+                    final v = int.parse(value!);
+                    if (v % 2 != 0) return "Must be an even integer";
+                    if (v < 202) return "Too small";
+                    return null;
+                  } catch (_) {
+                    return "Invalid value";
+                  }
+                },
+                autovalidateMode: AutovalidateMode.always,
+                keyboardType: TextInputType.number,
+                initialValue: targetDimension.toString(),
+                decoration: const InputDecoration(border: OutlineInputBorder(), isDense: true, suffixText: "px", labelText: "Maximum dimension"),
+              ),
+              Divider(),
+              Text("Log", style: Theme.of(context).textTheme.titleLarge),
+              Expanded(
+                child: SizedBox(
+                  width: double.maxFinite,
+                  child: SingleChildScrollView(
+                    controller: _controller,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 80.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: log.map((ln) => ln.trim().isEmpty ? const Divider() : Text(ln, style: TextStyle(fontFamily: "monospace"))).toList(),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           compressVideo(
-            widget.inputFile,
-            targetSize,
-            (String value) => setState(() {
+            inputFile: widget.inputFile,
+            targetSize: targetSize,
+            targetDimension: targetDimension,
+            logMessage: (String value) => setState(() {
               if (value.contains("\n")) {
                 log.add(value.replaceFirst(RegExp(r'\n$'), ''));
               } else {
--- lib/services/ffmpeg.dart
+++ lib/services/ffmpeg.dart
@@ -5,7 +5,9 @@ import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
 import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit_config.dart';
 import 'package:ffmpeg_kit_flutter_new/ffprobe_kit.dart';
 
-void compressVideo(String inputFile, num size, Function(String) logMessage) {
+void compressVideo({required String inputFile, required num targetSize, required num targetDimension, required Function(String) logMessage}) {
+  final num targetDim = targetDimension - (targetDimension % 2);
+
   String encoding;
 
   if (Platform.isAndroid) {
@@ -26,13 +28,18 @@ void compressVideo(String inputFile, num size, Function(String) logMessage) {
           final List<String> info = (await FFprobeKit.getMediaInformationFromCommand(
             '-v error -select_streams v:0 -show_entries format=duration:stream=width,height:side_data=rotation -read_intervals 0%+#1 -of default=noprint_wrappers=1 $inputFile',
           ).then((info) async => (await info.getOutput())!)).toString().split("\n");
-          final int rotation = int.parse(info.firstWhere((x) => x.startsWith("rotation=")).split("=")[1]);
+          int rotation;
+          try {
+            rotation = int.parse((info.firstWhere((x) => x.startsWith("rotation="))).split("=")[1]);
+          } catch (_) {
+            rotation = 0;
+          }
           final bool isLandscape = (rotation % 180).abs() != 0;
           final int width = int.parse(info.firstWhere((x) => x.startsWith(isLandscape ? "height=" : "width=")).split("=")[1]);
           final int height = int.parse(info.firstWhere((x) => x.startsWith(isLandscape ? "width=" : "height=")).split("=")[1]);
           final num duration = num.parse(info.firstWhere((x) => x.startsWith("duration=")).split("=")[1]);
 
-          final num target = size * 1024 * 1024;
+          final num target = targetSize * 1024 * 1024;
           final num totalBitrate = target / duration * 8;
           final num audioBitrate = min(64 * 1000, 0.2 * totalBitrate);
           final num videoBitrate = totalBitrate - audioBitrate;
@@ -49,11 +56,11 @@ void compressVideo(String inputFile, num size, Function(String) logMessage) {
             "-threads ${Platform.numberOfProcessors}",
           ];
 
-          if (max(width, height) > 1280) {
+          if (max(width, height) > targetDim) {
             if (width > height) {
-              args.add("-vf scale=1280:-2");
+              args.add("-vf scale=$targetDim:-2");
             } else {
-              args.add("-vf scale=-2:1280");
+              args.add("-vf scale=-2:$targetDim");
             }
           }
 
